63f93136277ab8182a59236ddf78462bd3679198
keremduran <keremduran@live.com>
2022-01-10 14:36:30 -0500

    Course Sections dynamic render

--- components/CourseSection.js
+++ components/CourseSection.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import CourseHeaderRecord from './CourseHeaderRecord';
+import CalloutRecord from './CalloutRecord';
+import LearnSectionRecord from './LearnSectionRecord';
+import PricingSectionRecord from './PricingSectionRecord';
 
-export default function CourseSection({ detail }) {
-  console.log(detail);
-  return (
-    <div>
-      <h1>Course Section</h1>
-      {detail.description}
-      <p>------------</p>
-    </div>
-  );
+export default function CourseSection({ details }) {
+  console.log(details);
+
+  const components = {
+    CourseHeaderRecord,
+    CalloutRecord,
+    LearnSectionRecord,
+    PricingSectionRecord,
+  };
+
+  const DynamicComponent = components[details.__typename];
+  return <DynamicComponent details={details} />;
+  // switch (details.__typename) {
+  //   case 'CourseHeaderRecord':
+  //     return <CourseHeaderRecord details={details} />;
+  //     break;
+  //   case 'CalloutRecord':
+  //     return <CalloutRecord details={details} />;
+  //     break;
+  //   case 'LearnSectionRecord':
+  //     return <LearnSectionRecord details={details} />;
+  //     break;
+  //   case 'PricingSectionRecord':
+  //     return <PricingSectionRecord details={details} />;
+  //     break;
+  //   default:
+  //     break;
+  // }
+  // if (details.__typename === 'CourseHeaderRecord') {
+  //   return <CourseHeaderRecord details={details} />;
+  // }
+  // return (
+  //   <div>
+  //     <h1>Course Section</h1>
+  //     {details.description}
+  //     <p>------------</p>
+  //   </div>
+  // );
 }
