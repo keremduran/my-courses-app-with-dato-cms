5bad54a042ae6dbf6f38ef218961243a8499bb52
keremduran <keremduran@live.com>
2022-01-11 13:08:23 -0500

    Header Design

--- components/CourseHeaderRecord.js
+++ components/CourseHeaderRecord.js
@@ -1,9 +1,38 @@
 import React from 'react';
-
-export default function CourseHeaderRecord() {
+import { Flex, Box, Center, Heading, Text, Container } from '@chakra-ui/react';
+export default function CourseHeaderRecord({ details }) {
   return (
-    <div>
-      <h1>Course Header</h1>
-    </div>
+    <Flex
+      direction='column'
+      alignItems='center'
+      textAlign='center'
+      bg='gray.200'
+      id='flex1'
+      py='20'
+      py='10'
+    >
+      <Heading
+        size='lg'
+        id='small-title'
+        color='gray.600'
+        fontWeight='extrabold'
+      >
+        {details.smalltitle}
+        <Text
+          as='span'
+          display='block'
+          fontSize='6xl'
+          id='big-title'
+          color='gray.900'
+          fontWeight='bold'
+          mb='10'
+        >
+          {details.bigtitle}
+        </Text>
+      </Heading>
+      <Container bg='yellow.300' maxW='container.md'>
+        <Text fontSize='lg'>{details.description}</Text>
+      </Container>
+    </Flex>
   );
 }
